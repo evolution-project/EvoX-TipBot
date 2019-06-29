efe2e31c4097fca5e3857390e5cdbb70c30f8446
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-06-29 22:57:03 +0200

    client

--- bot.js
+++ bot.js
@@ -289,13 +289,13 @@ function checkCommand(msg) {
 				var tiptarget = msg.mentions.users.first().id;
 
 				var myname = msg.author.username;
-				var user = msg.author.username;
+				var tiptarget = msg.client.username;
 
 				if (tiptarget != null) {
 					TipSomebody(msg, msg.author.id, tiptarget, user, myname, amount, function (success, message) {
 						if (success == true) {
 							msg.channel.send("<@" + tiptarget + "> has been tipped " + formatDisplayBalance(amount) + " " + coin_name + " :moneybag: by " + msg.author + custom_message);
-							msg.author.send("<@" + tiptarget + "> has been tipped " + formatDisplayBalance(amount) + " " + coin_name + " :moneybag: by " + msg.author + custom_message);
+							msg.client.send("<@" + tiptarget + "> has been tipped " + formatDisplayBalance(amount) + " " + coin_name + " :moneybag: by " + msg.author + custom_message);
 						} else { msg.channel.send(message); }
 
 					});
