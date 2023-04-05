885eedfa6d7e3c1f4591fd701bfbcf805d428f8a
cosmos <cosmos.1985@mail.ru>
2023-04-05 20:34:44 +0300

    work bot

--- bot_config.js
+++ bot_config.js
@@ -1,8 +1,8 @@
 module.exports = { 
 
-    wallethostname: "http://127.0.0.1:55055",
-    daemonhostname: "http://127.0.0.1:52521",
-    bot_token : "MTA5MTM1Mjk1OTY4Nzg2MDI0NA.GVatGM.xCicH_7rne7Je-EinQd4kjPCDAnQvp9pO5bRiY",
+    wallethostname: "http://138.2.160.222:55066",
+    daemonhostname: "http://138.2.160.222:52521",
+    bot_token : "MTA5MTM1Mjk1*******************************CnaK7WD0nbzVYeI0",
     mongodburl: "mongodb://127.0.0.1:27017/TipBot",
     
     owner_id : "939918743805497375",
@@ -10,11 +10,12 @@ module.exports = {
     block_maturity_requirement : 10,
     coin_total_units : 12,
     coin_display_units : 2,
-    server_wallet_address : "eX4miaQnsgQ7F3M148Ns6GBMqL1gK4RT6dt9EnorYtrEHPnXULGXjJaaPWhs5E3xCnNaTKRt7N13d3YzCLc4kQLg1JpPsv7a6",
-    withdraw_tx_fees : "1",
+    server_wallet_address : "eX3j4tksVENEFda6fzxd3oJygARxJG3Bm25uAKK3NKMi9MfLDyuWpQgjPVC8L6DhuD39S3YmQioH58m5FXUVLUK21BAFeLi9C",
+    withdraw_tx_fees : 0.01,
     withdraw_min_amount: "1",
     wait_time_for_withdraw_confirm : 30000,
     custom_message_length_limit: 100,
+    default_fee: 10000000000,
     log_1 : true,
     log_2 : true,
     log_3 : true
